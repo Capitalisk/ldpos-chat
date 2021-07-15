16961bbbfc7ef8cafb27b5dd7e535937edd8c702
Maarten Coppens <martinuske@gmail.com>
2021-07-15 11:36:54 -0300

    better sorting better layout for testing added knex debug

--- modules/messages.js
+++ modules/messages.js
@@ -1,11 +1,3 @@
-const sortByDate = (a, b) => {
-  a = new Date(a);
-  b = new Date(b);
-
-  if (a < b) return 1;
-  else return -1;
-};
-
 const attach = (agServer, socket, knex) => {
   (async () => {
     for await (const request of socket.procedure('messages/private')) {
@@ -13,20 +5,23 @@ const attach = (agServer, socket, knex) => {
         .select('*')
         .where({ toUserId: request.data.id })
         .orWhere({ ownerId: request.data.id })
-        .leftJoin('users', 'messages.ownerId', 'users.id');
+        .innerJoin('users', 'messages.ownerId', 'users.id')
+        .orderBy('createdAt');
 
-      request.end(messages.sort(sortByDate));
+      request.end(messages);
     }
   })();
 
   (async () => {
     for await (const request of socket.procedure('messages/channel')) {
+      // users override createdAt of messages. This is a fix for that, see https://github.com/knex/knex/issues/61#issuecomment-139267190
       const messages = await knex('messages')
-        .select('*')
+        .select(['users.*', 'messages.*'])
         .where({ channelId: request.data.id })
-        .leftJoin('users', 'messages.ownerId', 'users.id');
+        .leftJoin('users', 'messages.ownerId', 'users.id')
+        .orderBy('createdAt');
 
-      request.end(messages.sort(sortByDate));
+      request.end(messages);
     }
   })();
 };
